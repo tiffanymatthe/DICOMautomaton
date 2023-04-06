19706f08f880f2767019ded55cf941f2e7171b97
dominicklukas <klukasdominic@gmail.com>
2023-04-06 13:58:15 -0700

    Upgraded CreateCustomContour

--- src/Operations/CreateCustomContour.cc
+++ src/Operations/CreateCustomContour.cc
@@ -10,6 +10,8 @@
 
 #include "CreateCustomContour.h"
 
+using namespace std;
+
 OperationDoc OpArgDocCreateCustomContour(){
     OperationDoc out;
     out.name = "CreateCustomContour";
@@ -61,56 +63,88 @@ bool CreateCustomContour(Drover &DICOM_data,
     YLOGINFO("The Y values are " << YValues);
     YLOGINFO("The Z values are " << ZValues);
 
-    contour_of_points<double> cop;
     contour_collection<double> cc;
 
-    cop.closed = true;
+    vector<string> x_contours = SplitString(XValues, "|");
+    vector<string> y_contours = SplitString(YValues, "|");
+    vector<string> z_contours = SplitString(ZValues, "|");
+    
 
-    std::vector<double> x_values = ValueStringToDoubleList(XValues);
-    std::vector<double> y_values = ValueStringToDoubleList(YValues);
-    std::vector<double> z_values = ValueStringToDoubleList(ZValues);
-
-    if(x_values.size()!=y_values.size()||y_values.size()!=z_values.size()) {
-        YLOGWARN("Each list must have the same number of numbers");
+    if(x_contours.size()!=y_contours.size()||y_contours.size()!=z_contours.size()) {
+        YLOGWARN("Ensure same number of contours for each dimension");
         return true;
     }
 
-    if(x_values.size() < 3) {
-        YLOGWARN("We require more than 3 points to be in a contour");
+    if(x_contours.size() < 1) {
+        YLOGWARN("We require that there is at least one contour.");
         return true;
     }
-
-    for(int i = 0; i < x_values.size(); i++) {
-        const vec3<double> p(static_cast<double>(x_values[i]), 
-                            static_cast<double>(y_values[i]),
-                            static_cast<double>(z_values[i]));
-        cop.points.emplace_back(p);
-    }
-    cop.metadata["ROILabel"] = ROILabel;
-
-    cc.contours.emplace_back(cop);
 
     DICOM_data.Ensure_Contour_Data_Allocated();
+    
+    vector<vector<double>> x_cops;
+    vector<vector<double>> y_cops;
+    vector<vector<double>> z_cops;
+
+    for(int i = 0; i < x_contours.size(); i++) {
+        vector<string> x_cop_string = SplitString(x_contours[i], " ");
+        vector<string> y_cop_string = SplitString(y_contours[i], " ");
+        vector<string> z_cop_string = SplitString(z_contours[i], " ");
+        if(x_cop_string.size()!=y_cop_string.size()||y_cop_string.size()!=z_cop_string.size()) {
+            YLOGWARN("Ensure each contour has the same number of points for each dimension");
+        }
+        if(x_cop_string.size() < 3) {
+            YLOGWARN("Each contour must have at least 3 points");
+        }
+        vector<double> x_cop;
+        vector<double> y_cop;
+        vector<double> z_cop;
+        for(int j = 0; j < x_cop_string.size(); j++) {
+            try {
+                x_cop.emplace_back(stod(x_cop_string[j]));
+                y_cop.emplace_back(stod(y_cop_string[j]));
+                z_cop.emplace_back(stod(z_cop_string[j]));
+                string error_message = "Invalid Input";
+            } catch(string error_message) {
+                return true;
+            }
+        }
+        x_cops.emplace_back(x_cop);
+        y_cops.emplace_back(y_cop);
+        z_cops.emplace_back(z_cop);
+    }
+
+    for(int i = 0; i < x_cops.size(); i++) {
+        contour_of_points<double> cop;
+        for(int j = 0; j < x_cops[i].size(); j++) {
+            const vec3<double> p(static_cast<double>(x_cops[i][j]),
+                                static_cast<double>(y_cops[i][j]),
+                                static_cast<double>(z_cops[i][j]));
+            cop.points.emplace_back(p);
+        }
+        cop.closed = true;
+        cop.metadata["ROILabel"] = ROILabel;
+        cc.contours.emplace_back(cop);
+    }
+
     DICOM_data.contour_data->ccs.emplace_back(cc);
     return true;
 }
 
-std::vector<double> ValueStringToDoubleList(std::string input) {
-    std::vector<double> values;
+std::vector<string> SplitString(std::string input, std::string delimiter) {
+    vector<string> values;
     size_t pos = 0;
-    std::string delimiter = " ";
-    std::string token;
+    string token;
     while ((pos = input.find(delimiter)) != std::string::npos) {
         token = input.substr(0, pos);
         try {
-            values.emplace_back(stod(token));
-            std::string error_message = "Invalid Input";
-        } catch(std::string error_message) {
-            YLOGWARN(error_message);
-            return std::vector<double>();
+            values.emplace_back(token);
+            string error_message = "Invalid Input";
+        } catch(string error_message) {
+            return std::vector<string>();
         }
         input.erase(0, pos + delimiter.length());
     }
+    values.emplace_back(input);
     return values;
 }
-
